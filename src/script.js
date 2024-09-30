fade632ea454b2c92ad639ad8deb999c57d9be61
Adryan Melo <adryan.george.ti@gmail.com>
2024-09-30 18:55:36 -0300

    primeiro commit

---
+++ src/script.js
@@ -0,0 +1,19 @@
+let myBtn = document.getElementById("myBtn");
+let modal = document.getElementById("modal");
+let invalidFeed = document.querySelector("div.invalid-feedback");
+let progressBar = document.getElementById("progressBar");
+
+
+myBtn.addEventListener('click',()=>{
+
+
+  // setTimeout(()=>{
+  //   modal.style.top = "5%";
+  //   progressBar.style.width = "0%"
+  //   setTimeout(()=>{
+  //     modal.style.top = "-25%"
+  //     progressBar.style.width = "100%"
+  //   },3000)
+    
+  // },1000)
+})
